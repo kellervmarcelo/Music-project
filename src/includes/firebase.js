d455c701f6d9616519846192689812a63db09a84
Marcelo Keller <33509703+kellervmarcelo@users.noreply.github.com>
2021-07-17 18:48:57 -0300

    Primeiro commit
    Estão feitas as partes de upload, de validação de campos e de rotas

---
+++ src/includes/firebase.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzOVcNtymMnRjRfekh29EDhRvAqQEJT9s',
+  authDomain: 'music-4a88b.firebaseapp.com',
+  projectId: 'music-4a88b',
+  storageBucket: 'music-4a88b.appspot.com',
+  appId: '1:376209977341:web:c3e2ca028e1b8ca123f394',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
+
+export {
+  auth,
+  db,
+  usersCollection,
+  songsCollection,
+  storage,
+};
---
+++ src/views/Manage.vue
@@ -0,0 +1,70 @@
+<template>
+  <!-- Main Content -->
+  <section class="container mx-auto mt-6">
+    <div class="md:grid md:grid-cols-3 md:gap-4">
+      <div class="col-span-1">
+        <AppUpload/>
+      </div>
+      <div class="col-span-2">
+        <div
+          class="bg-white rounded border border-gray-200 relative flex flex-col"
+        >
+          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+            <span class="card-title">My Songs</span>
+            <i
+              class="fa fa-compact-disc float-right text-green-400 text-2xl"
+            ></i>
+          </div>
+          <div class="p-6">
+            <!-- Composition Items -->
+            <composition-item v-for="song in songs" :key="song.docID"
+              :song="song" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// import store from '@/store/index';
+import AppUpload from '@/components/Upload.vue';
+import CompositionItem from '@/components/CompositionItem.vue';
+import { songsCollection, auth } from '@/includes/firebase';
+
+export default {
+  name: 'manage',
+  components: {
+    AppUpload,
+    CompositionItem,
+  },
+  data() {
+    return {
+      songs: [],
+    };
+  },
+  async created() {
+    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get();
+
+    snapshot.forEach((document) => {
+      const song = {
+        ...document.data(),
+        docID: document.id,
+      };
+      this.songs.push(song);
+    });
+  },
+  // beforeRouteLeave(to, from, next) {
+  //   this.$refs.upload.cancelUploads();
+  //   next();
+  // },
+  // beforeRouteEnter(to, from, next) {
+  //   if (store.state.userLoggedIn) {
+  //     next();
+  //   } else {
+  //     next({ name: 'home' });
+  //   }
+  // },
+};
+
+</script>
