56df711a59352140f99ca092e1003ae85654879c
Marcelo Keller <33509703+kellervmarcelo@users.noreply.github.com>
2021-07-27 20:21:17 -0300

    Aplicativo finalizado
    Terminada as rotas, bem como adicionado o lib Howler.js para tocar as músicas dentro do app

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
+import Song from '@/views/Song.vue';
 import store from '@/store';
 
 const routes = [
@@ -27,6 +28,11 @@ const routes = [
     //   console.log('Manage route guard');
     //   next();
     // },
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song,
   },
   {
     path: '/manage',
