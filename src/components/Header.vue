9868577ea6f3c17fa9b09cf9457fcfc2f3b69879
Marcelo Keller <33509703+kellervmarcelo@users.noreply.github.com>
2021-08-01 02:42:18 -0300

    Feitas algumas otimizações no app
    Mudar a importação dos components para usar chunks, auto-importação de components globais, de rotas, separação da store em modules e etc.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -52,7 +52,9 @@ import { mapMutations, mapState } from 'vuex';
 export default {
   name: 'Header',
   computed: {
-    ...mapState(['userLoggedIn']),
+    ...mapState({
+      userLoggedIn: (state) => state.auth.userLoggedIn,
+    }),
     currentLocale() {
       return this.$i18n.locale === 'pt' ? 'Português' : 'English';
     },
